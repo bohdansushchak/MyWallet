d4254a720e91f81d0e3d973d23cf9cbc99c6259f
Bohdan Sushchak <bogdansushak@gmail.com>
2019-06-08 21:58:21 +0200

    Implemented saving categories, orders and product into firestore

---
+++ app/src/main/java/bohdan/sushchak/mywallet/data/firebase/ApiDatabase.kt
@@ -0,0 +1,17 @@
+package bohdan.sushchak.mywallet.data.firebase
+
+import bohdan.sushchak.mywallet.data.db.entity.CategoryEntity
+import bohdan.sushchak.mywallet.data.db.entity.OrderEntity
+import bohdan.sushchak.mywallet.data.db.entity.ProductEntity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+
+
+interface ApiDatabase {
+
+    suspend fun addCategory(categoryEntity: CategoryEntity): DocumentReference
+    suspend fun addOrder(orderEntity: OrderEntity, productsEntity: List<ProductEntity>): DocumentReference
+
+    
+
+}
