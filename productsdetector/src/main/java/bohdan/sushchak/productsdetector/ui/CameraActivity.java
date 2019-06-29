eff63680b9cffeadbe7afb2c67c71e2e4d5c1d87
Bohdan Sushchak <bogdansushak@gmail.com>
2019-06-29 18:57:37 +0200

    Implemented detection item

--- productsdetector/src/main/java/bohdan/sushchak/productsdetector/ui/CameraActivity.java
+++ productsdetector/src/main/java/bohdan/sushchak/productsdetector/ui/CameraActivity.java
@@ -287,13 +287,10 @@ public abstract class CameraActivity extends AppCompatActivity
         if (useCamera2API) {
             CameraConnectionFragment camera2Fragment =
                     CameraConnectionFragment.newInstance(
-                            new CameraConnectionFragment.ConnectionCallback() {
-                                @Override
-                                public void onPreviewSizeChosen(final Size size, final int rotation) {
-                                    previewHeight = size.getHeight();
-                                    previewWidth = size.getWidth();
-                                    CameraActivity.this.onPreviewSizeChosen(size, rotation);
-                                }
+                            (size, rotation) -> {
+                                previewHeight = size.getHeight();
+                                previewWidth = size.getWidth();
+                                CameraActivity.this.onPreviewSizeChosen(size, rotation);
                             },
                             this,
                             DESIRED_PREVIEW_SIZE);
---
+++ productsdetector/src/main/java/bohdan/sushchak/productsdetector/views/DetectedItem.kt
@@ -0,0 +1,63 @@
+package bohdan.sushchak.productsdetector.views
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import bohdan.sushchak.productsdetector.R
+import kotlinx.android.synthetic.main.detected_item.view.*
+
+@SuppressLint("ViewConstructor")
+class DetectedItem(
+    context: Context,
+    attrs: AttributeSet?
+) : FrameLayout(context, attrs) {
+
+    init {
+        View.inflate(context, R.layout.detected_item, this)
+
+        attrs?.let {
+            val attributes = context.theme
+                .obtainStyledAttributes(attrs, R.styleable.DetectedItem, 0, 0)
+            try {
+                initAttrs(attributes)
+            } finally {
+                attributes.recycle()
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initAttrs(attrs: TypedArray) {
+        val backgroundColor = attrs.getColor(R.styleable.DetectedItem_backgroundColor, Color.TRANSPARENT)
+        var backgroundDrawable = attrs.getDrawable(R.styleable.DetectedItem_background)
+        val label = attrs.getString(R.styleable.DetectedItem_label)
+        val accracy = attrs.getString(R.styleable.DetectedItem_accuracy)
+
+        if (backgroundDrawable == null)
+            backgroundDrawable = context.getDrawable(R.drawable.detected_item_bg)
+
+        backgroundDrawable?.setColorFilter(backgroundColor, PorterDuff.Mode.OVERLAY)
+        rootView.background = backgroundDrawable
+
+        tvDetectedItemName.text = label
+        tvDetectedItemAccuracy.text = "$accracy%"
+    }
+
+    fun setOnButtonClickListener(onClickListener: OnClickListener) {
+        iBtnAddItem.setOnClickListener(onClickListener)
+    }
+
+    fun setLabel(label: String) {
+        tvDetectedItemName.text = label
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun setAccuracy(accuracy: Int) {
+        tvDetectedItemAccuracy.text = "$accuracy%"
+    }
+}
